a10aca5a5c436541566bc14a7fdb3dece8e7ca03
IAMAKJOD <95494174+IAMAKJOD@users.noreply.github.com>
2023-11-05 09:42:26 +0530

    Update seg.py

--- seg.py
+++ seg.py
@@ -54,6 +54,8 @@ if uploaded_image is not None:
     slic_segments = slic(image, n_segments=100, compactness=10)
     slic_segmented = label2rgb(slic_segments, image=image, kind='avg')
 
+    # Rescale the slic_segmented image to [0.0, 1.0] range
+    slic_segmented = slic_segmented / 255.0
+
     # Display SLIC Superpixel Segmentation result
     st.image(slic_segmented, caption='Graph-Based Segmentation (SLIC)', use_column_width=True)
-    
